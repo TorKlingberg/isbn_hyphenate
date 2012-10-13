ca1466dfae7bffe42134e1e8818bfd6d0f8f3ad0
TorKlingberg <tor.klingberg@gmail.com>
2012-10-13 23:18:09 +0200

    New version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'isbn_hyphenate',
     packages = ['isbn_hyphenate'],
-    version = '1',
+    version = '1.0.1',
     description = 'a Python library to add hyphens in the right place to an ISBN (International Standard Book Number)',
     author = 'Tor Klingberg',
     author_email = 'tor.klingberg@gmail.com',
